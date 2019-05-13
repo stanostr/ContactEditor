9fc541dae719d04795a9b151882f4a1dfbac2451
stanostr <stan_ostr@yahoo.com>
2019-05-13 14:33:55 -0400

    Fixed tests.

--- test/my/contacteditor/ContactRepositoryImplTest.java
+++ test/my/contacteditor/ContactRepositoryImplTest.java
@@ -15,7 +15,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
  * @author stan_
  */
 public class ContactRepositoryImplTest {
@@ -25,7 +24,7 @@ public class ContactRepositoryImplTest {
         repo = new ContactRepositoryImpl();
         jdbc = JDBCWrapper.get();
         jdbc.update("Delete from contact", null);
-        repo.save(new Contact("Silky", "Ostrovskii", "718-710-5433", "silky@aol.com", "Future wife", "Fiancee"));
+        repo.save(new Contact("Silky", "Ostrovskii", "123-345-6789", "silky@abc.com", "Future wife", "Fiancee"));
     }
     
     @BeforeClass
@@ -48,8 +47,8 @@ public class ContactRepositoryImplTest {
      * Test of getContactByEmail method, of class ContactRepositoryImpl.
      */
     @Test
-    public void testGetContactByEmail() {
-        Contact silky = repo.getContactByEmail("silky@aol.com");
+    public void testGetContactByEmail() throws SQLException {
+        Contact silky = repo.getContactByEmail("silky@abc.com");
         System.out.println(silky.getFirstname() + " " + silky.getLastname());
         assertEquals(silky.getFirstname(), "Silky"); 
     }
@@ -58,11 +57,11 @@ public class ContactRepositoryImplTest {
      * Test of save method, of class ContactRepositoryImpl.
      */
     @Test
-    public void testSave() {
+    public void testSave() throws SQLException {
         System.out.println("save");
-        Contact contact = new Contact("Sveni", "Svenson", "123-123-1234", "sven@aol.com", "Sven", "The Skullcrusher");
+        Contact contact = new Contact("Sveni", "Svenson", "123-123-1234", "sven@abc.com", "Sven", "The Skullcrusher");
         repo.save(contact);
-        Contact sven = repo.getContactByEmail("sven@aol.com");
+        Contact sven = repo.getContactByEmail("sven@abc.com");
         System.out.println(sven.getFirstname() + " " + sven.getLastname());
         assertEquals(sven.getFirstname(), "Sveni");
        
@@ -72,12 +71,12 @@ public class ContactRepositoryImplTest {
      * Test of update method, of class ContactRepositoryImpl.
      */
     @Test
-    public void testUpdate() {
+    public void testUpdate() throws SQLException {
         System.out.println("update");
-        Contact contact = repo.getContactByEmail("silky@aol.com");
-        contact.setEmail("silky2@aol.com");
+        Contact contact = repo.getContactByEmail("silky@abc.com");
+        contact.setEmail("silky2@abc.com");
         repo.update(contact);
-        Contact contact2 = repo.getContactByEmail("silky2@aol.com");
+        Contact contact2 = repo.getContactByEmail("silky2@abc.com");
         System.out.println(contact2.getFirstname() + " " + contact2.getLastname());
         assertEquals(contact2.getFirstname(), "Silky");
 
@@ -87,16 +86,16 @@ public class ContactRepositoryImplTest {
      * Test of delete method, of class ContactRepositoryImpl.
      */
     @Test
-    public void testDelete() {
+    public void testDelete() throws SQLException {
         System.out.println("delete");
-        Contact contact = repo.getContactByEmail("silky@aol.com");
+        Contact contact = repo.getContactByEmail("silky@abc.com");
         repo.delete(contact);
-        Contact deletedContact = repo.getContactByEmail("silky@aog.com");
+        Contact deletedContact = repo.getContactByEmail("silky@abc.com");
         assertEquals(deletedContact, null);
     }  
     
     @Test
-    public void testFindAll()
+    public void testFindAll() throws SQLException
     {
         List<Contact> list = repo.findAll();
         System.out.println("Found " + list.size() + " contacts");
